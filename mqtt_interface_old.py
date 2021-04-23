1ff2db5758a29b1ab9d6c74985291ddf5c608538
Ian Kline <kline.ian.d@gmail.com>
2021-04-22 23:04:16 -0400

    updates

--- mqtt_interface_old.py
+++ mqtt_interface_old.py
@@ -28,160 +28,74 @@ map = {
     "Bedroom": {
         "portals": ["Master Bath", "Hallway"],
         "circuits": ["E2","F1","G1","G2"],
-        "rgb": ["windowpi"],
-        "buttons": [],
-        "onbuttons": [],
-        "offbuttons": [],
-        "rgbbuttons": [],
-        "labels": []
+        "rgb": ["windowpi"]
     },
     "Master Bath": {
         "portals": ["Bedroom"],
         "circuits": ["I1","J2"],
-        "rgb": [],
-        "buttons": [],
-        "onbuttons": [],
-        "offbuttons": [],
-        "rgbbuttons": [],
-        "labels": []
+        "rgb": []
     },
     "Hallway": {
         "portals": ["Bedroom","Bathroom","Library","Workout Room","Living Room"],
         "circuits": ["J1"],
-        "rgb": [],
-        "buttons": [],
-        "onbuttons": [],
-        "offbuttons": [],
-        "rgbbuttons": [],
-        "labels": []
+        "rgb": []
     },
     "Bathroom": {
         "portals": ["Hallway"],
         "circuits": ["H1"],
-        "rgb": [],
-        "buttons": [],
-        "onbuttons": [],
-        "offbuttons": [],
-        "rgbbuttons": [],
-        "labels": []
+        "rgb": []
     },
     "Library": {
         "portals": ["Hallway"],
         "circuits": [],
-        "rgb": [],
-        "buttons": [],
-        "onbuttons": [],
-        "offbuttons": [],
-        "rgbbuttons": [],
-        "labels": []
+        "rgb": []
     },
     "Workout Room": {
         "portals": ["Hallway"],
         "circuits": [],
-        "rgb": [],
-        "buttons": [],
-        "onbuttons": [],
-        "offbuttons": [],
-        "rgbbuttons": [],
-        "labels": []
+        "rgb": []
     },
     "Living Room": {
         "portals": ["Hallway","Kitchen","Dining Room"],
         "circuits": ["A1","B1","C1"],
-        "rgb": ["canvaspi"],
-        "buttons": [],
-        "onbuttons": [],
-        "offbuttons": [],
-        "rgbbuttons": [],
-        "labels": []
+        "rgb": ["canvaspi"]
     },
     "Kitchen": {
         "portals": ["Living Room","Shop","Dining Room"],
         "circuits": ["D2","F2"],
-        "rgb": [],
-        "buttons": [],
-        "onbuttons": [],
-        "offbuttons": [],
-        "rgbbuttons": [],
-        "labels": []
+        "rgb": []
     },
     "Dining Room": {
         "portals": ["Kitchen","Game Room","Office"],
         "circuits": ["C2"],
-        "rgb": [],
-        "buttons": [],
-        "onbuttons": [],
-        "offbuttons": [],
-        "rgbbuttons": [],
-        "labels": []
+        "rgb": []
     },
     "Game Room": {
         "portals": ["Dining Room","Deck"],
         "circuits": [],
-        "rgb": ["clockpi","gameroompi"],
-        "buttons": [],
-        "onbuttons": [],
-        "offbuttons": [],
-        "rgbbuttons": [],
-        "labels": []
+        "rgb": ["clockpi","gameroompi"]
     },
     "Office": {
         "portals": ["Dining Room","Deck"],
         "circuits": ["D1","E1"],
-        "rgb": [],
-        "buttons": [],
-        "onbuttons": [],
-        "offbuttons": [],
-        "rgbbuttons": [],
-        "labels": []
+        "rgb": []
     },
     "Shop": {
         "portals": ["Kitchen","Laundry Room","Electronics Room"],
         "circuits": ["H2","I2"],
-        "rgb": [],
-        "buttons": [],
-        "onbuttons": [],
-        "offbuttons": [],
-        "rgbbuttons": [],
-        "labels": []
+        "rgb": []
     },
     "Laundry Room": {
         "portals": ["Shop"],
         "circuits": [],
-        "rgb": [],
-        "buttons": [],
-        "onbuttons": [],
-        "offbuttons": [],
-        "rgbbuttons": [],
-        "labels": []
+        "rgb": []
     },
     "Electronics Room": {
         "portals": ["Shop"],
         "circuits": [],
-        "rgb": [],
-        "buttons": [],
-        "onbuttons": [],
-        "offbuttons": [],
-        "rgbbuttons": [],
-        "labels": []
+        "rgb": []
     }
 }
-
-colors = [
-    {"label":"white","command":"0:255:255:255"},
-    {"label":"red","command":"0:255:0:0"},
-    {"label":"green","command":"0:0:255:0"},
-    {"label":"blue","command":"0:0:0:255"},
-    {"label":"orange","command":"0:255:64:0"},
-    {"label":"purple","command":"0:255:0:255"},
-    {"label":"teal","command":"0:0:255:255"},
-    {"label":"off","command":"0:0:0:0"},
-    {"label":"gradient","command":"3:0"},
-    {"label":"rainbow","command":"4:0"},
-    {"label":"random pixels","command":"5:0"},
-    {"label":"random change","command":"6:0"},
-    {"label":"swipe change","command":"7:0"}
-]
 
 myname = socket.gethostname()
 ############# CONFIG #############
@@ -193,18 +107,10 @@ current_room = "Living Room"
 client = mqtt.Client()
 window = tk.Tk()
 window.attributes("-fullscreen", 1)
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
-column_width = screen_width / 4
-window.columnconfigure(0, minsize=column_width)
-window.columnconfigure(1, minsize=column_width)
-window.columnconfigure(2, minsize=column_width)
-window.columnconfigure(3, minsize=column_width)
-
 greetingvar = tk.StringVar()
-greetingvar.set(current_room)
-greeting = tk.Label(textvariable=greetingvar, font=("Times", 24))
-greeting.grid(row=0, column=1, sticky="nesw", pady=15)
+greetingvar.set('MQTT Control Switch - '+current_room)
+greeting = tk.Label(textvariable=greetingvar, bg="black", fg="white")
+greeting.grid(row=0, columnspan=4)
 
 def mosquittoDo(topic, command):
     try:
@@ -239,8 +145,8 @@ def gotoroom(roomname):
     global current_room
     global greetingvar
     room = map[current_room]
-    # for button in room["buttons"]:
-    #     button.grid_forget()
+    for button in room["buttons"]:
+        button.grid_forget()
     for button in room["rgbbuttons"]:
         button.grid_forget()
     for button in room["onbuttons"]:
@@ -250,58 +156,75 @@ def gotoroom(roomname):
     for label in room["labels"]:
         label.grid_forget()
     current_room = roomname
-    greetingvar.set(current_room)
+    greetingvar.set('MQTT Control Switch - '+current_room)
     room = map[current_room]
-    # r = 1
-    # for button in room["buttons"]:
-    #     button.grid(row=r, column=0)
-    #     r = r + 1
+    r = 1
+    for button in room["buttons"]:
+        button.grid(row=r, column=0)
+        r = r + 1
     r = 1
     for label in room["labels"]:
-        label.grid(row=r, column=1, sticky="ew", padx=2, pady=2)
+        label.grid(row=r, column=1)
         r = r + 1
     r = 1
     for button in room["onbuttons"]:
-        button.grid(row=r, column=2, sticky="ew", padx=2, pady=2)
+        button.grid(row=r, column=2)
         r = r + 1
     r = 1
     for button in room["offbuttons"]:
-        button.grid(row=r, column=3, sticky="ew", padx=2, pady=2)
+        button.grid(row=r, column=3)
         r = r + 1
     c = 2
     for button in room["rgbbuttons"]:
-        button.grid(row=r, column=c, sticky="ew", padx=2, pady=2)
+        button.grid(row=r, column=c)
         if c == 2:
             c = 3
         else:
             r = r + 1
             c = 2
-#button = []
+button = []
 onbutton = []
 offbutton = []
 label = []
 b = 0
 nb = 0
+colors = [
+    {"label":"white","command":"0:255:255:255"},
+    {"label":"red","command":"0:255:0:0"},
+    {"label":"green","command":"0:0:255:0"},
+    {"label":"blue","command":"0:0:0:255"},
+    {"label":"orange","command":"0:255:64:0"},
+    {"label":"purple","command":"0:255:0:255"},
+    {"label":"teal","command":"0:0:255:255"},
+    {"label":"off","command":"0:0:0:0"},
+    {"label":"gradient","command":"3:0"},
+    {"label":"rainbow","command":"4:0"},
+    {"label":"random pixels","command":"5:0"},
+    {"label":"random change","command":"6:0"},
+    {"label":"swipe change","command":"7:0"}
+]
 for roomkey in map.keys():
     room = map[roomkey]
-    button = tk.Button(text=roomkey,command=lambda id=roomkey: room_click(id), height=button_height, width=button_width, font=("Times", 16))
-    button.grid(row=b+1, column=0, sticky="ew", padx=2, pady=2)
-    b = b + 1
-    # for portal in room["portals"]:
-    #     button.append(tk.Button(text=portal,command=lambda id=portal: room_click(id), height=button_height, width=button_width))
-    #     map[roomkey]["buttons"].append(button[b])
-    #     b = b + 1
+    map[roomkey]["buttons"] = []
+    map[roomkey]["onbuttons"] = []
+    map[roomkey]["offbuttons"] = []
+    map[roomkey]["rgbbuttons"] = []
+    map[roomkey]["labels"] = []
+    for portal in room["portals"]:
+        button.append(tk.Button(text=portal,command=lambda id=portal: room_click(id), height=button_height, width=button_width))
+        map[roomkey]["buttons"].append(button[b])
+        b = b + 1
     for cid in room["circuits"]:
         circuit = circuits[cid]
-        label.append(tk.Label(text=circuit["label"], font=("Times", 16)))
-        onbutton.append(tk.Button(text="ON",command=lambda id=cid: on_click(id), height=button_height, width=button_width, font=("Times", 16)))
-        offbutton.append(tk.Button(text="OFF",command=lambda id=cid: off_click(id), height=button_height, width=button_width, font=("Times", 16)))
+        label.append(tk.Label(text=circuit["label"]))
+        onbutton.append(tk.Button(text="ON",command=lambda id=cid: on_click(id), height=button_height, width=button_width))
+        offbutton.append(tk.Button(text="OFF",command=lambda id=cid: off_click(id), height=button_height, width=button_width))
         map[roomkey]["onbuttons"].append(onbutton[nb])
         map[roomkey]["offbuttons"].append(offbutton[nb])
         map[roomkey]["labels"].append(label[nb])
         nb = nb + 1
     for rgb in room["rgb"]:
         for color in colors:
-            map[roomkey]["rgbbuttons"].append(tk.Button(text=color["label"],command=lambda id=rgb, command=color["command"]: rgb_click(id, command), height=button_height, width=button_width, font=("Times", 16)))
+            map[roomkey]["rgbbuttons"].append(tk.Button(text=color["label"],command=lambda id=rgb, command=color["command"]: rgb_click(id, command), height=button_height, width=button_width))
 gotoroom(current_room)
 window.mainloop()
