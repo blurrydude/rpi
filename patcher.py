7ff29e5c0e039fde072277ffb08250182d50e4a0
Ian Kline <kline.ian.d@gmail.com>
2021-07-19 21:58:57 -0400

    updates

--- patcher.py
+++ patcher.py
@@ -18,12 +18,12 @@ def sms(message):
         return
     account_sid = 'AC26cbcaf937e606af51c6a384728a4e75' 
     auth_token = '7b8e7b5be6d3e4c2246d9f8ed5156ddc' 
-    with Client(account_sid, auth_token) as client:
-        client.messages.create(  
-            messaging_service_sid='MG1cf18075f26dc8ff965a5d2d1940dab5', 
-            body=message,      
-            to='+19377166465' 
-        ) 
+    client = Client(account_sid, auth_token)
+    client.messages.create(  
+        messaging_service_sid='MG1cf18075f26dc8ff965a5d2d1940dab5', 
+        body=message,      
+        to='+19377166465' 
+    ) 
 
 lookup = {
     "rpi4-web-server": "app",
