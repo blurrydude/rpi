226216429386268e1f054517708ed1ff45958b3d
Ian D Kline <idkline@winsupplyinc.com>
2021-07-19 23:37:56 -0400

    updates

--- new_app.py
+++ new_app.py
@@ -41,7 +41,8 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 @app.route('/debug',methods=['POST'])
 def debug():
     with open('SMS.json','w') as write_file:
-        write_file.write(json.dumps(request.values.get('Body',None)))
+        write_file.write(json.dumps(request.json))
+    return 'OK'
 
 @app.route('/control/<text>')
 def control(text):
