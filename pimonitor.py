9abc5679c6efb6e68d1787db694391322f89ebe9
Ian Kline <kline.ian.d@gmail.com>
2021-07-20 18:31:42 -0400

    updates

--- pimonitor.py
+++ pimonitor.py
@@ -25,7 +25,7 @@ def on_message(client, userdata, message):
         "ip": ip,
         "heartbeat": timestamp
     }
-    with open("pistates.json") as write_file:
+    with open("/home/pi/pistates.json") as write_file:
         write_file.write(json.dumps(pi))
 
 if __name__ == "__main__":
