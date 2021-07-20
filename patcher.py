6397a80ca1e11e6cac2d9b7cc8bf430d9e776a3d
Ian D Kline <idkline@winsupplyinc.com>
2021-07-19 21:28:55 -0400

    updates

--- patcher.py
+++ patcher.py
@@ -11,7 +11,7 @@ webserver = myname == "rpi4-web-server"
 whitenoise = myname == "whitenoisepi"
 
 if webserver is True:
-    import twilio.rest
+    from twilio.rest import Client
     account_sid = 'AC26cbcaf937e606af51c6a384728a4e75' 
     auth_token = '7b8e7b5be6d3e4c2246d9f8ed5156ddc' 
     client = Client(account_sid, auth_token) 
