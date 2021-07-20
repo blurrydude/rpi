6e9871e9787283b16a5a7b156504a913cbad0ee7
Ian D Kline <idkline@winsupplyinc.com>
2021-07-19 21:37:49 -0400

    updates

--- new_app.py
+++ new_app.py
@@ -10,7 +10,7 @@ account_sid = 'AC26cbcaf937e606af51c6a384728a4e75'
 auth_token = '7b8e7b5be6d3e4c2246d9f8ed5156ddc' 
 client = Client(account_sid, auth_token) 
 
-f = open('/home/pi/rpi/circuits.json',)
+f = open('/home/pi/rpi/circuits.json')
 circuits = json.load(f)
 
 def mosquittoDo(topic, command):
--- patcher.py
+++ patcher.py
@@ -47,16 +47,13 @@ def doCheck():
     repo_version_file = '/home/pi/rpi/version.json'
     local_version_file = '/home/pi/version.json'
     with open(repo_version_file, "r") as read_file:
-        repo_version_json = read_file.read()
+        repo_version = json.load(read_file)
     if path.exists(local_version_file) == False:
         with open(local_version_file, "w") as write_file:
-            write_file.write(repo_version_json)
+            write_file.write(json.dumps(repo_version))
 
     with open(local_version_file, "r") as read_file:
-        local_version_json = read_file.read()
-
-    repo_version = json.load(repo_version_json)
-    local_version = json.load(local_version_json)
+        local_version = json.load(read_file)
 
     now = datetime.datetime.now()
     if local_version[whatiuse] != repo_version[whatiuse] or (now.hour == 0 and now.minute == 0 and webserver is False and whitenoise is False):
