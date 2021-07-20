d6731545f4dea9ad2551c57bfb4c6df5d0897c91
Ian Kline <kline.ian.d@gmail.com>
2021-07-19 21:57:54 -0400

    updates

--- patcher.py
+++ patcher.py
@@ -12,18 +12,18 @@ whitenoise = myname == "whitenoisepi"
 
 if webserver is True:
     from twilio.rest import Client
-    account_sid = 'AC26cbcaf937e606af51c6a384728a4e75' 
-    auth_token = '7b8e7b5be6d3e4c2246d9f8ed5156ddc' 
-    client = Client(account_sid, auth_token) 
 
 def sms(message):
     if webserver is False:
         return
-    client.messages.create(  
-        messaging_service_sid='MG1cf18075f26dc8ff965a5d2d1940dab5', 
-        body=message,      
-        to='+19377166465' 
-    ) 
+    account_sid = 'AC26cbcaf937e606af51c6a384728a4e75' 
+    auth_token = '7b8e7b5be6d3e4c2246d9f8ed5156ddc' 
+    with Client(account_sid, auth_token) as client:
+        client.messages.create(  
+            messaging_service_sid='MG1cf18075f26dc8ff965a5d2d1940dab5', 
+            body=message,      
+            to='+19377166465' 
+        ) 
 
 lookup = {
     "rpi4-web-server": "app",
