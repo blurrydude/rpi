3cec416aabe6eb82243455babdf2605e459b7650
Ian D Kline <idkline@winsupplyinc.com>
2021-07-19 21:01:25 -0400

    updates

--- new_app.py
+++ new_app.py
@@ -4,7 +4,11 @@ from flask_cors import CORS
 import paho.mqtt.client as mqtt
 import time
 import json
-
+from twilio.rest import Client 
+ 
+account_sid = 'AC26cbcaf937e606af51c6a384728a4e75' 
+auth_token = '7b8e7b5be6d3e4c2246d9f8ed5156ddc' 
+client = Client(account_sid, auth_token) 
 
 f = open('/home/pi/rpi/circuits.json',)
 circuits = json.load(f)
@@ -17,6 +21,14 @@ def mosquittoDo(topic, command):
     client.publish(topic,command)
     client.disconnect()
     return 'OK'
+
+@app.route('/sms/<message>')
+def sms(message):
+    client.messages.create(  
+        messaging_service_sid='MG1cf18075f26dc8ff965a5d2d1940dab5', 
+        body=message,      
+        to='+19377166465' 
+    ) 
 
 app = FlaskAPI(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
