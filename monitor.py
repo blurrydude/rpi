f3c4457e08f8f2f6b0f2dee13d7bcd241613ab58
Ian Kline <kline.ian.d@gmail.com>
2021-07-20 16:39:50 -0400

    updates

---
+++ monitor.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+import time
+time.sleep(15)
+import paho.mqtt.client as mqtt
+import json
+
+running = True
+f = open('/home/pi/rpi/circuits.json')
+circuits = json.load(f)
+
+client = mqtt.Client()
+
+def on_message(client, userdata, message):
+    global running
+    result = str(message.payload.decode("utf-8"))
+    #print("Received: "+result)
+    bits = result.split('/')
+    address = bits[1]
+    with open("/home/pi/"+address+".state") as write_file:
+        write_file.write(result)
+    #print(check)
+
+if __name__ == "__main__":
+    client.on_message = on_message
+    client.connect('192.168.1.22')
+    for circuit in circuits:
+        relays = 1
+        if "shellyswitch25" in circuit["address"]:
+            relays = 2
+        for i in range(relays):
+            topic = 'shellies/'+circuit["address"]+'/relay/'+str(i)
+            client.subscribe(topic)
+    client.loop_start()
+    while running is True:
+        time.sleep(1)
+    client.loop_stop()
+    client.disconnect()
--- mqtt_rgb.py
+++ mqtt_rgb.py
@@ -47,10 +47,6 @@ elif myname == "namepi":
     num_pixels = 8
     rainbow_cycle_delay = 0.05
     mqtt_enabled = False
-elif myname == "hallwaypi":
-    num_pixels = 1
-    rainbow_cycle_delay = 0.001
-    mqtt_enabled = True
 
 for i in range(0,num_pixels):
     current_colors.append((0,0,0))
