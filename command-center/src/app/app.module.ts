cfe4da91e8e45c659e4d72d576cec36c896c6374
Ian Kline <kline.ian.d@gmail.com>
2021-07-26 10:39:03 -0400

    updates

--- command-center/src/app/app.module.ts
+++ command-center/src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './pages/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule,
-    CommonModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- command-center/src/app/pages/home/home.component.ts
+++ command-center/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 import { StatusService } from "../../services/status.service";
 
 @Component({
@@ -6,7 +6,7 @@ import { StatusService } from "../../services/status.service";
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-    status: object = {};
+    @Output() status: object = {};
     constructor(private httpMessageService: StatusService) { 
       this.httpMessageService.getStatus().toPromise().then(msg => {
         this.status = msg;
