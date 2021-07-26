db8cefb187dbf0cdb16b6bc8627c1bfd6ebc90b9
Ian D Kline <idkline@winsupplyinc.com>
2021-07-25 22:22:43 -0400

    updates

--- command-center/src/app/app.module.ts
+++ command-center/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- command-center/src/app/pages/home/home.component.ts
+++ command-center/src/app/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { concat } from 'rxjs';
 import { StatusService } from "../../services/status.service";
 
 @Component({
@@ -7,7 +6,7 @@ import { StatusService } from "../../services/status.service";
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-    status: object;
+    status: object = {};
     constructor(private httpMessageService: StatusService) { 
       this.httpMessageService.getStatus().toPromise().then(msg => {
         this.status = msg;
