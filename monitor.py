55a168350ceea89f56b726294377a36d1fd6eee2
Ian Kline <kline.ian.d@gmail.com>
2021-07-30 20:17:06 -0400

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -3,8 +3,11 @@ import time
 time.sleep(15)
 import paho.mqtt.client as mqtt
 import json
+import requests
 
 running = True
+fs = open('/home/pi/rpi/motionsensors.json')
+sensors = json.load(fs)
 f = open('/home/pi/rpi/circuits.json')
 circuits = json.load(f)
 
@@ -12,6 +15,25 @@ client = mqtt.Client()
 
 def on_message(client, userdata, message):
     global running
+    if "status" in message.topic:
+        result = json.loads(str(message.payload.decode("utf-8")))
+        print(result)
+        # {
+        # "motion": true,
+        # "timestamp": 1627686399,
+        # "active": true,
+        # "vibration": false,
+        # "lux": 0,
+        # "bat": 94
+        # }
+        if result["motion"] is True:
+            for sensor in sensors:
+                if sensor["address"] in message.topic:
+                    sendCommand("turn on "+sensor["activate_light"])
+        if result["motion"] is False:
+            for sensor in sensors:
+                if sensor["address"] in message.topic and sensor["auto_off"] is True:
+                    sendCommand("turn off "+sensor["activate_light"])
     result = str(message.payload.decode("utf-8"))
     #print("Received: "+result)
     bits = message.topic.split('/')
@@ -26,6 +48,14 @@ def on_message(client, userdata, message):
             print(address + " " + relay + " " + result)
             write_file.write(result)
     #print(check)
+    
+def sendCommand(command):
+    print("sending command: "+command)
+    try:
+        r =requests.get('https://api.idkline.com/control/'+command)
+        print(str(r.status_code))
+    except:
+        print('failed to send command')
 
 if __name__ == "__main__":
     client.on_message = on_message
@@ -41,6 +71,10 @@ if __name__ == "__main__":
             topic = 'shellies/'+circuit["address"]+'/relay/'+str(i)+'/power'
             print('subscribing to '+topic)
             client.subscribe(topic)
+    for sensor in sensors:
+        topic = 'shellies/'+sensor["address"]+'/status'
+        print('subscribing to '+topic)
+        client.subscribe(topic)
     client.loop_start()
     while running is True:
         time.sleep(1)
