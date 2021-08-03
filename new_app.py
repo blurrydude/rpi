85a7dd8be7252a1f09908d0ba65e41a9be379e68
Ian D Kline <idkline@winsupplyinc.com>
2021-08-03 02:52:28 -0400

    updates

--- new_app.py
+++ new_app.py
@@ -121,7 +121,7 @@ def gettoken():
         }
         with open("/home/pi/users.json","w") as write_file:
             json.dump(users,fp=write_file)
-    r = request.args
+    r = request.get_json(force=True)
     username = r["username"]
     passhash = r["passhash"]
     check_user = None
