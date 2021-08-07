26c2e037675bd6b627b102b9b9ae7674e060da76
Ian D Kline <idkline@winsupplyinc.com>
2021-08-07 14:15:53 -0400

    updates

--- new_app.py
+++ new_app.py
@@ -300,9 +300,13 @@ def control(text):
     elif "shade" in command:
         if "open" in command:
             command_list.append({"t":"pi/rollerpi/commands","c":"0:0"})
+            command_list.append({"t":"pi/rollerpi/commands","c":"1:0"})
+            command_list.append({"t":"pi/rollerpi/commands","c":"2:0"})
             text = text + "opening the shades\n"
         else:
             command_list.append({"t":"pi/rollerpi/commands","c":"0:1"})
+            command_list.append({"t":"pi/rollerpi/commands","c":"1:1"})
+            command_list.append({"t":"pi/rollerpi/commands","c":"2:1"})
             text = text + "closing the shades\n"
 
     elif "shop door" in command:
