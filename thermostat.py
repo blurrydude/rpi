d13f95f4ce1a75ef4993aab7fb3660a45a2e2275
Ian D Kline <idkline@winsupplyinc.com>
2021-08-16 22:20:35 -0400

    updates

--- thermostat.py
+++ thermostat.py
@@ -200,10 +200,11 @@ def cycle():
     report_readings()
 
     if circulating is True:
-        if humidity > base_humidity: # the humidity should fall for a bit, but when it starts to come back up, that means the air from the basement has arrived
+        if humidity > base_humidity - 0.2: # the humidity should fall for a bit, but when it starts to come back up, that means the air from the basement has arrived
         # if datetime.now() > circulate_until: # kept for reference and so I can toggle which way this works for ... some reason.
             stop_circulating()
         else:
+            base_humidity = humidity
             return
 
     if delay_stage > datetime.now():
