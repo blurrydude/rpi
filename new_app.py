f83ef8f171141f5310db39eba166446256458608
Ian D Kline <idkline@winsupplyinc.com>
2021-08-20 15:12:33 -0400

    updates

--- new_app.py
+++ new_app.py
@@ -599,7 +599,15 @@ def getsysmonlogbydate(logfiledate):
 @app.route('/gettemplog')
 def gettemplog():
     logfiledate = datetime.now().strftime("%Y%m%d")
-    return gettemplogbydate(logfiledate)
+    h = datetime.now().hour - 3
+    data = []
+    while h <= datetime.now().hour:
+        hour = str(h)
+        if h < 10:
+            hour = "0" + hour
+        a = gettemplogbydate(logfiledate + hour)
+        data = data + a
+    return data
 
 @app.route('/gettemplog/<logfiledate>')
 def gettemplogbydate(logfiledate):
