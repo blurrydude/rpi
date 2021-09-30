cbd68467c45449fc925990081864d1666da511b0
Ian D Kline <idkline@winsupplyinc.com>
2021-09-29 23:19:17 -0400

    Update main.py

--- SmarterCircuits/main.py
+++ SmarterCircuits/main.py
@@ -137,7 +137,7 @@ class SmarterCircuitsMCP:
             data = datetime.now().strftime("%Y%m%d%H%M")
             logfile = os.path.dirname(os.path.realpath(__file__))+"/logs/Thermostat_"+(datetime.now()).strftime("%Y%m%d")+".log"
             for thermo in self.thermostats:
-                data = data + ":" + thermo.name + "=" + self.binarize(thermo.state.heat_on) + self.binarize(thermo.state.ac_on) + self.binarize(thermo.state.fan_on) + self.binarize(thermo.state.whf_on) + "|" + str(thermo.state.temperature) + "|" + str(thermo.state.humidity)
+                data = data + ":" + thermo.room + "=" + self.binarize(thermo.state.heat_on) + self.binarize(thermo.state.ac_on) + self.binarize(thermo.state.fan_on) + self.binarize(thermo.state.whf_on) + "|" + str(thermo.state.temperature) + "|" + str(thermo.state.humidity)
             mode = "a"
             if os.path.exists(logfile) is False:
                 mode = "w"
