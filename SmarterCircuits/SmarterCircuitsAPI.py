2d365033e535e35690bfb04b03298e0b8adbf08b
Ian Kline <kline.ian.d@gmail.com>
2021-10-13 16:27:23 -0400

    updates

--- SmarterCircuits/SmarterCircuitsAPI.py
+++ SmarterCircuits/SmarterCircuitsAPI.py
@@ -14,11 +14,49 @@ class SmarterAPI:
 
         @self.app.route('/pistates',methods=['GET'])
         def pistates():
-            return self.mcp.peers
+            output = []
+            for peer in self.mcp.peers:
+                output.append({
+                    "id": peer.id,
+                    "name": peer.name,
+                    "ip_address": peer.ip_address,
+                    "model": peer.model,
+                    "circuit_authority": peer.circuit_authority,
+                    "timestamp": peer.timestamp,
+                    "thermostat": peer.thermostat,
+                    "rollershade": peer.rollershade,
+                    "rollerdoor": peer.rollerdoor
+                })
+            return output
         
         @self.app.route('/states',methods=['GET'])
         def states():
-            return self.mcp.config.circuits
+            output = []
+            for circuit in self.mcp.config.circuits:
+                output.append({
+                    "id": self.id,
+                    "ip_address": self.ip_address,
+                    "name": self.name,
+                    "relay_id": self.relay_id,
+                    "rollershutter": self.rollershutter,
+                    "location": self.location,
+                    "zones": self.zones,
+                    "on_modes": self.on_modes,
+                    "off_modes": self.off_modes,
+                    "status": {
+                        "relay": {
+                            "on": self.status.relay.on,
+                            "power": self.status.relay.power,
+                            "energy": self.status.relay.energy
+                        },
+                        "temperature": self.status.temperature,
+                        "temperature_f": self.status.temperature_f,
+                        "overtemperature": self.status.overtemperature,
+                        "temperature_status": self.status.temperature_status,
+                        "voltage": self.status.voltage
+                    }
+                })
+            return output
         
         @self.app.route('/control/<text>')
         def control(text):
