2073acf3e2f70fd12662a6d507e1309533183629
Ian D Kline <idkline@winsupplyinc.com>
2022-04-01 18:14:30 -0400

    updates

--- SmarterCircuits/SmarterCircuitsPassiveMonitor.py
+++ SmarterCircuits/SmarterCircuitsPassiveMonitor.py
@@ -6,64 +6,8 @@ import time
 import pyautogui
 import tkinter as tk
 import _thread
-import numpy
-import pyaudio
-import math
+import beepy
  
- 
-class ToneGenerator(object):
- 
-    def __init__(self, samplerate=44100, frames_per_buffer=4410):
-        self.p = pyaudio.PyAudio()
-        self.samplerate = samplerate
-        self.frames_per_buffer = frames_per_buffer
-        self.streamOpen = False
- 
-    def sinewave(self):
-        if self.buffer_offset + self.frames_per_buffer - 1 > self.x_max:
-            # We don't need a full buffer or audio so pad the end with 0's
-            xs = numpy.arange(self.buffer_offset,
-                              self.x_max)
-            tmp = self.amplitude * numpy.sin(xs * self.omega)
-            out = numpy.append(tmp,
-                               numpy.zeros(self.frames_per_buffer - len(tmp)))
-        else:
-            xs = numpy.arange(self.buffer_offset,
-                              self.buffer_offset + self.frames_per_buffer)
-            out = self.amplitude * numpy.sin(xs * self.omega)
-        self.buffer_offset += self.frames_per_buffer
-        return out
- 
-    def callback(self, in_data, frame_count, time_info, status):
-        if self.buffer_offset < self.x_max:
-            data = self.sinewave().astype(numpy.float32)
-            return (data.tostring(), pyaudio.paContinue)
-        else:
-            return (None, pyaudio.paComplete)
- 
-    def is_playing(self):
-        if self.stream.is_active():
-            return True
-        else:
-            if self.streamOpen:
-                self.stream.stop_stream()
-                self.stream.close()
-                self.streamOpen = False
-            return False
- 
-    def play(self, frequency, duration, amplitude):
-        self.omega = float(frequency) * (math.pi * 2) / self.samplerate
-        self.amplitude = amplitude
-        self.buffer_offset = 0
-        self.streamOpen = True
-        self.x_max = math.ceil(self.samplerate * duration) - 1
-        self.stream = self.p.open(format=pyaudio.paFloat32,
-                                  channels=1,
-                                  rate=self.samplerate,
-                                  output=True,
-                                  frames_per_buffer=self.frames_per_buffer,
-                                  stream_callback=self.callback)
-
 class SmarterCircuitsPassiveMonitor:
     def __init__(self):
         self.mqtt = SmarterCircuitsMQTT.SmarterMQTTClient(["192.168.2.200"],["notifications"],self.on_message)
@@ -71,7 +15,6 @@ class SmarterCircuitsPassiveMonitor:
         self.window = tk.Tk()
         self.labels = []
         self.display_on = False
-        self.tonegen = ToneGenerator()
         self.start()
 
     def start(self):
@@ -114,18 +57,19 @@ class SmarterCircuitsPassiveMonitor:
             os.system("echo 'on 0.0.0.0' | cec-client -s -d 1")
             self.display_on = True
         time.sleep(5)
-        print('\a')
-        self.tonegen.play(440, 1, 0.5)
+        beep(sound="ping")
         time.sleep(0.5)
-        self.tonegen.play(880, 1, 0.5)
+        beep(sound="ping")
         time.sleep(0.5)
-        self.tonegen.play(990, 1, 0.5)
+        beep(sound="ping")
         time.sleep(0.5)
-        self.tonegen.play(440, 1, 0.5)
+        beep(sound="ping")
         time.sleep(0.5)
-        self.tonegen.play(880, 1, 0.5)
+        beep(sound="ping")
         time.sleep(0.5)
-        self.tonegen.play(990, 1, 0.5)
+        beep(sound="ping")
+        time.sleep(0.5)
+        beep(sound="ping")
 
     def screen_close_timer(self):
         time.sleep(90)
