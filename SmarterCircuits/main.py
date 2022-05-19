c9922375c59f7aa45380db22628edbbd0ee07a64
Ian D Kline <idkline@winsupplyinc.com>
2022-05-19 15:26:34 -0400

    Update main.py

--- SmarterCircuits/main.py
+++ SmarterCircuits/main.py
@@ -653,11 +653,14 @@ class SmarterCircuitsMCP:
                 self.battery_status_check(sensor)
         if subtopic == "sensor/temperature":
             sensor.status.temperature = float(message)
-            self.send_discord_message(self.discord_house_room, sensor.name+" temperature is "+message+" F")
+            if self.circuit_authority is True:
+                self.send_discord_message(self.discord_house_room, sensor.name+" temperature is "+message+" F")
 
         if subtopic == "sensor/humidity":
             sensor.status.humidity = float(message)
             self.send_discord_message(self.discord_house_room, sensor.name+" humidity is "+message+" %")
+            if self.circuit_authority is True:
+                self.send_discord_message(self.discord_house_room, sensor.name+" temperature is "+message+" F")
 
     def handle_shelly_motion_message(self, id, subtopic, message):
         sensor = self.config.motion_sensors[id]
