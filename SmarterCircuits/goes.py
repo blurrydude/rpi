bcbea1423586e2c51fa9bc3da55c4571fb5b6fcf
blurrydude <kline.ian.d@gmail.com>
2022-06-16 00:56:17 -0400

    Update goes.py

--- SmarterCircuits/goes.py
+++ SmarterCircuits/goes.py
@@ -1,77 +1,101 @@
 import math
 import os
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import requests
 from io import BytesIO
 from datetime import datetime, timedelta
 
 save_path = "C:/Temp/GOES/"
 last_grab = datetime.now() - timedelta(minutes=5)
+pixels_per_mile = 3
+data = []
+home = (1085,1350)
+myFont = ImageFont.truetype(save_path+'Ubuntu-Regular.ttf', 24)
 
 def grab_images():
-    goes16baseUrl = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/cgl/" #16/latest.jpg"
     now = datetime.now()
     nowstr = now.strftime("%Y%m%d%H%M")
     for i in range(1,17):
         index = str(i)
         if i < 10:
             index = "0"+index
-        url = goes16baseUrl+index+"/latest.jpg"
-        print("GRAB: "+url)
-        response = requests.get(url)
-        img = Image.open(BytesIO(response.content))
-        img.save(save_path+index+"-"+nowstr+".jpg")
+        grab_image(index,save_path+index+"-"+nowstr+".jpg")
+
+def grab_image(band,save_file):
+    goes16baseUrl = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/cgl/"
+    url = goes16baseUrl+band+"/latest.jpg"
+    print("GRAB: "+url)
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    img.save(save_file)
 
 def run():
+    global last_grab
     while True:
-        if last_grab < datetime.now() - timedelta(minutes=5):
-            last_grab = datetime.now()
-            grab_images()
+        now = datetime.now()
+        if last_grab < now - timedelta(minutes=5):
+            last_grab = now
+            grab_and_process()
+
+def grab_and_process():
+    now = datetime.now()
+    nowstr = now.strftime("%Y%m%d%H%M")
+    file_path = save_path+"11-"+nowstr+".jpg"
+    grab_image("11",file_path)
+    process_band_11_image(file_path)
 
 def process_band_11():
-    frames = []
     for filename in os.listdir(save_path):
         f = os.path.join(save_path, filename)
         # checking if it is a file
-        c = 0
         if os.path.isfile(f) and filename.startswith("11-"):
-            img = Image.open(f)
-            frames.append([])
-            for y in range(img.height):
-                frames[c].append([])
-                for x in range(img.width):
-                    p = img.getpixel((x,y))
-                    r = p[0]
-                    g = p[1]
-                    b = p[2]
-                    rgd = abs(r-g)
-                    rbd = abs(r-b)
-                    if rgd < 60 and rbd < 60:
-                        frames[c][y].append(0)
-                    else:
-                        frames[c][y].append(r)
-    home = (1085,1350)
+            process_band_11_image(f)
+    
+def process_band_11_image(f):
+    global data
     mass_in_range = 0
-    for frame in frames:
-        w = len(frame[0])
-        h = len(frame)
-        img = Image.new(mode="RGB",size=(w,h))
-        for y in range(h):
-            for x in range(w):
-                d = math.dist(home,(x,y))/3
-                r = 0
-                if frame[y][x] >= 200:
-                    r = 255
-                g = 0
-                b = int(max(0,255 - d))
-                if b > 0 and r > 0:
-                    g = 255
-                    mass_in_range = mass_in_range + 1
-                img.putpixel((x,y),(r,g,b))
-
-        print("storm mass in range: "+str(mass_in_range))
-        draw_cross_hair(home,img)
-        img.show()
+    closest_mass = 1000000
+    print("reading band 11 image")
+    img = Image.open(f)
+    w = img.width
+    h = img.height
+    nimg = Image.new(mode="RGB",size=(w,h))
+    print("image is "+str(w)+" by "+str(h))
+    for y in range(h):
+        for x in range(w):
+            d = math.dist(home,(x,y))/2
+            p = img.getpixel((x,y))
+            r = p[0]
+            g = p[1]
+            b = p[2]
+            nr = 0
+            ng = 0
+            nb = int(max(0,255 - d))
+            rgd = abs(r-g)
+            rbd = abs(r-b)
+            if rgd < 60 and rbd < 60:
+                nimg.putpixel((x,y),(nr,ng,nb))
+                continue
+            if r >= 200:
+                nr = 255
+                miles = round(d * pixels_per_mile)
+                if miles < closest_mass:
+                    closest_mass = miles
+            if nb > 0 and nr > 0:
+                ng = 255
+                mass_in_range = mass_in_range + 1
+            nimg.putpixel((x,y),(nr,ng,nb))
+    if closest_mass == 1000000:
+        closest_mass = 0
+    imgdraw = ImageDraw.Draw(nimg)
+    imgdraw.text((28, 32), "mass in range: "+str(mass_in_range), font=myFont, fill=(255, 255, 255))
+    imgdraw.text((28, 64), "closest mass: "+str(closest_mass)+" miles", font=myFont, fill=(255, 255, 255))
+    data.append({
+        "mass_in_range":mass_in_range,
+        "closest_mass":closest_mass,
+    })
+    draw_cross_hair(home,nimg)
+    nimg.save(f.replace("11-","processed"))
 
 def draw_cross_hair(xy,img):
     x = xy[0]
@@ -91,4 +115,4 @@ def draw_cross_hair(xy,img):
     img.putpixel((x,y+3),(0,255,0))
 
 if __name__ == "__main__":
-    process_band_11()
+    run()
