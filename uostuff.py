3496c957c5826e5c065e2af26c90a0de0fd52003
Ian D Kline <idkline@winsupplyinc.com>
2022-07-17 09:07:26 -0400

    Update uostuff.py

--- uostuff.py
+++ uostuff.py
@@ -1,8 +1,9 @@
+from audioop import minmax
 from PIL import Image
 import os
 
-#directory = "C:\\Users\\idkline\\Dropbox\\UOStuff\\UOArt\\Items\\"
-directory = "/home/ian/Dropbox/UOStuff/UOArt/Items/"
+directory = "C:\\Users\\idkline\\Dropbox\\UOStuff\\UOArt\\Items\\"
+#directory = "/home/ian/Dropbox/UOStuff/UOArt/Items/"
 def get_item_image(id):
     f = os.path.join(directory, str(id)+".png")    
     item = Image.open(f)
@@ -13,21 +14,64 @@ def cart_to_iso(point):
     isoY = (point[0] + point[1])/2
     return [isoX, isoY]
 
-def mappos_to_screen_pos(x, y):
-    iso = cart_to_iso((x, y))
-    return (int(iso[0]), int(iso[1]))
+def mappos_to_screen_pos(x, y, h = 0):
+    rsx = (y * -22) + (x * 22)
+    rsy = (y * 22) + (x * 22) - h
+    return (rsx, rsy)
 
-img = Image.new(mode="RGBA", size=(1024,768))
-tile = get_item_image(1173)
-sx = 0
-sy = 0
+def add_item(img, item, screen_pos):
+    img.paste(item, screen_pos, item)
+items = {}
+rowstart = (0,0)
+points = []
+minx = 0
+maxx = 0
+maxy = 0
 for y in range(10):
-    #screen_pos = mappos_to_screen_pos(x*44, y*22)
-    sx = sx - 22
-    sy = sy + 22
+    # #screen_pos = mappos_to_screen_pos(x*44, y*22)
+    # rowstart = (
+    #     rowstart[0] - 22,
+    #     rowstart[1] + 22
+    # )
+    # screen_pos = rowstart
+    points.append([])
     for x in range(10):
-        sx = sx + 22
-        sy = sy + 22
-        screen_pos = (sx, sy)
-        img.paste(tile, screen_pos, tile)
+        screen_pos = mappos_to_screen_pos(x, y)
+        #img.paste(tile, screen_pos, tile)
+        points[y].append([screen_pos[0], screen_pos[1], 1180])
+        if screen_pos[0] < minx:
+            minx = screen_pos[0]
+        if screen_pos[0] > maxx:
+            maxx = screen_pos[0]
+        if screen_pos[1] > maxy:
+            maxy = screen_pos[1]
+        # screen_pos = (
+        #     screen_pos[0] + 22,
+        #     screen_pos[1] + 22
+        # )
+maxx = maxx - minx
+items_to_load = []
+for xpoints in points:
+    for point in xpoints:
+        if point[2] not in items_to_load:
+            items_to_load.append(point[2])
+
+for item in items_to_load:
+    items[str(item)] = get_item_image(item)
+
+img = Image.new(mode="RGBA", size=(maxx+44,maxy+44))
+for xpoints in points:
+    for point in xpoints:
+        screen_pos = (
+            point[0] - minx,
+            point[1]
+        )
+        add_item(img, items[str(point[2])], screen_pos)
+wall = get_item_image(2475)
+point = mappos_to_screen_pos(9,0,wall.height-44)
+screen_pos = (
+    point[0] - minx,
+    point[1]
+)
+add_item(img, wall, screen_pos)
 img.show()
