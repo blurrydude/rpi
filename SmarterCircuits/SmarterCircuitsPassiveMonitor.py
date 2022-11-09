2aa07c03e75649b9e7f3c0c56effad0e9a268f25
blurrydude <kline.ian.d@gmail.com>
2022-11-08 21:05:35 -0500

    Update SmarterCircuitsPassiveMonitor.py

--- SmarterCircuits/SmarterCircuitsPassiveMonitor.py
+++ SmarterCircuits/SmarterCircuitsPassiveMonitor.py
@@ -23,7 +23,7 @@ class SmarterCircuitsPassiveMonitor:
         self.display_on = False
         self.screen_timer = 0
         self.font_size = 64
-        self.line_char_width = 72
+        self.line_char_width = 56
         self.lines = 9
         self.last_display = datetime.now()
         _thread.start_new_thread(self.sleep_timer, ())
