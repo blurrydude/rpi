e96ee4bf14c1d84aec2529bdff5458656a6efd60
BlurryDude <dude@blurrydude.com>
2023-03-26 19:09:28 -0400

    update

--- test_ha.py
+++ test_ha.py
@@ -1,6 +1,6 @@
 import requests
 import json
-state_raw = {
+state = {
     "ac_on": False,
     "fan_on": False,
     "heat_on": False,
@@ -9,35 +9,38 @@ state_raw = {
     "temperature": 70.9,
     "whf_on": False
 }
-
-state = {"state": "70.9", "attributes": {"unit_of_measurement": "°F"}}
-
-settings = {
-    "air_circulation_minutes": 10,
-    "circulation_cycle_minutes": 10,
-    "failed_read_halt_limit": 10,
-    "humidity_setting": 0,
-    "stage_cooldown_minutes": 10,
-    "stage_limit_minutes": 20,
-    "swing_temp_offset": 2,
-    "system_disabled": False,
-    "temperature_high_setting": 76,
-    "temperature_low_setting": 72,
-    "use_whole_house_fan": False,
-    "ventilation_cycle_minutes": 10
-}
-
-data = {
-    "room": "hallway",
-    "state": state,
-    "settings": settings
-}
+room = "hallway"
+ac_on = False
+fan_on = False
+heat_on = False
+whf_on = False
 
 with open('/home/ian/ha_token') as f:
     ha_token = f.read().strip()
 
-response = requests.post(f"http://192.168.2.82:8123/api/states/thermostat.hallway",json.dumps(state),headers={
+tstate = {"state": str(state["temperature"]), "attributes": {"unit_of_measurement": "°F"}}
+hstate = {"state": str(state["humidity"]), "attributes": {"unit_of_measurement": "%"}}
+print(requests.post(f"http://192.168.2.82:8123/api/states/sensor.{room}_thermostat_temperature",json.dumps(tstate),headers={
     "Authorization": f"Bearer {ha_token}",
     "content-type": "application/json",
-})
-print(response)
+}))
+print(requests.post(f"http://192.168.2.82:8123/api/states/sensor.{room}_thermostat_humidity",json.dumps(hstate),headers={
+    "Authorization": f"Bearer {ha_token}",
+    "content-type": "application/json",
+}))
+print(requests.post(f"http://192.168.2.82:8123/api/states/binary_sensor.{room}_heat_on",json.dumps({"state":heat_on}),headers={
+    "Authorization": f"Bearer {ha_token}",
+    "content-type": "application/json",
+}))
+print(requests.post(f"http://192.168.2.82:8123/api/states/binary_sensor.{room}_ac_on",json.dumps({"state":ac_on}),headers={
+    "Authorization": f"Bearer {ha_token}",
+    "content-type": "application/json",
+}))
+print(requests.post(f"http://192.168.2.82:8123/api/states/binary_sensor.{room}_fan_on",json.dumps({"state":fan_on}),headers={
+    "Authorization": f"Bearer {ha_token}",
+    "content-type": "application/json",
+}))
+print(requests.post(f"http://192.168.2.82:8123/api/states/binary_sensor.{room}_whf_on",json.dumps({"state":whf_on}),headers={
+    "Authorization": f"Bearer {ha_token}",
+    "content-type": "application/json",
+}))
